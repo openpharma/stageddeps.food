42f4b554e62b2d8fbd6e1977218d6817c55cbba0
Nikolas Burkoff <nikolas.burkoff@roche.com>
2021-11-22 09:02:57 +0000

    add renv

--- R/hello.R
+++ R/hello.R
@@ -5,3 +5,16 @@ get_food <- function() {
   stageddeps.electricity::get_electricity()
   print("Producing food")
 }
+
+
+#' Dummy function to use IRanges in package
+#' @export
+use_IRanges <- function() {
+  IRanges::IRanges(start=1:10, width=10:1)
+}
+
+#' Dummy function to use yaml in package
+#' @export
+use_yaml <- function() {
+  yaml::yaml.load(yaml.load("foo: 123\nbar: 456"))
+}
